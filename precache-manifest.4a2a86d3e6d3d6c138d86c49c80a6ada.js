d9fde2be0b9f49eb11164aefdbb058f051b8558d
markdatanepal <prismco.biz@gmail.com>
2021-01-19 15:23:36 +0545

    Updates

---
+++ precache-manifest.4a2a86d3e6d3d6c138d86c49c80a6ada.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c76620fe7477d4257fee901d2908786",
+    "url": "/markdata/index.html"
+  },
+  {
+    "revision": "8be799c9dd639891be7a",
+    "url": "/markdata/static/css/main.9c6b5dd8.chunk.css"
+  },
+  {
+    "revision": "0c464ec03eede22c3dcc",
+    "url": "/markdata/static/js/2.107485a5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/markdata/static/js/2.107485a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8be799c9dd639891be7a",
+    "url": "/markdata/static/js/main.e96633a5.chunk.js"
+  },
+  {
+    "revision": "6776348aeb8eefb3cc8c",
+    "url": "/markdata/static/js/runtime-main.298c6bad.js"
+  },
+  {
+    "revision": "c6f5c4c52697f60ef6d24332e5f2a524",
+    "url": "/markdata/static/media/1.c6f5c4c5.jpg"
+  },
+  {
+    "revision": "5159a5d89abe8bf68b09b569dbeccbc0",
+    "url": "/markdata/static/media/IBMPlexSans-Bold.5159a5d8.ttf"
+  },
+  {
+    "revision": "ee425cc83f37323665790c89758cf359",
+    "url": "/markdata/static/media/IBMPlexSans-BoldItalic.ee425cc8.ttf"
+  },
+  {
+    "revision": "40bbef08ca6f6edea2a9a9e882541ce0",
+    "url": "/markdata/static/media/IBMPlexSans-Italic.40bbef08.ttf"
+  },
+  {
+    "revision": "ee83103a4a777209b0f759a4ff598066",
+    "url": "/markdata/static/media/IBMPlexSans-Medium.ee83103a.ttf"
+  },
+  {
+    "revision": "eb7dadea8e7c37ce1a1406045dda7c1e",
+    "url": "/markdata/static/media/IBMPlexSans-MediumItalic.eb7dadea.ttf"
+  },
+  {
+    "revision": "c02b4dc6554c116e4c40f254889d5871",
+    "url": "/markdata/static/media/IBMPlexSans-Regular.c02b4dc6.ttf"
+  },
+  {
+    "revision": "50f13391babe5b650b8a0589dd962f74",
+    "url": "/markdata/static/media/facebook.50f13391.svg"
+  },
+  {
+    "revision": "baf48a32fd45986dfb5b52aada1b558a",
+    "url": "/markdata/static/media/github.baf48a32.svg"
+  },
+  {
+    "revision": "a50716dd845ea43407a9a1bcffe42aef",
+    "url": "/markdata/static/media/twitter.a50716dd.svg"
+  }
+]);
